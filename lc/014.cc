e58c0602d9ae0d02a320ac554665df8f9e2c214a
Sergei Radutnuy <sergeiradutnuy01@gmail.com>
2017-03-14 00:24:27 -0700

    014 6ms 43.02% c++

---
+++ lc/014.cc
@@ -0,0 +1,20 @@
+class Solution {
+ public:
+  string longestCommonPrefix(const vector<string> &strs) {
+    if (strs.empty())
+      return "";
+    if (strs.size() == 1)
+      return strs[0];
+
+    int lastIndex = strs[0].size();
+    for (const string &s : strs) {
+      for (int i = 0; i <= lastIndex; ++i) {
+        if (strs[0][i] != s[i]) {
+          lastIndex = i;
+          break;
+        }
+      }
+    }
+    return strs[0].substr(0, lastIndex);
+  }
+};
