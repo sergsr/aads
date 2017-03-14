800398c7f741e5913506f032fce52d1e9cdfd139
Sergei Radutnuy <sergeiradutnuy01@gmail.com>
2017-03-14 00:27:55 -0700

    026 29ms 53.28% c++

---
+++ lc/026.cc
@@ -0,0 +1,17 @@
+class Solution {
+public:
+  int removeDuplicates(vector<int> &nums) {
+    if (nums.empty()) return 0;
+
+    int length = 1;
+    int previous = nums[0];
+    for (int i = 1; i < nums.size(); ++i) {
+      if (nums[i] != previous) {
+        nums[length] = nums[i];
+        ++length;
+      }
+      previous = nums[i];
+    }
+    return length;
+  }
+};
