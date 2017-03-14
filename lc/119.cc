5b7f15dba2f05b633a595e081ab74e26b17eb30b
Sergei Radutnuy <sergeiradutnuy01@gmail.com>
2017-03-14 00:39:20 -0700

    119 3ms 3.60% c++

---
+++ lc/119.cc
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> row;
+
+        long n = rowIndex + 1;
+        long elem = 1;
+        for (long k = 0; k < n; ++k) {
+            row.push_back(elem);
+            elem *= (n - k - 1);
+            elem /= (k + 1);
+        }
+
+        return row;
+    }
+};
