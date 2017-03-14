a039ceb9955fde181192898e6a70c4bebb5d249f
Sergei Radutnuy <sergeiradutnuy01@gmail.com>
2017-03-14 00:49:04 -0700

    344 12ms 22.58% c++

---
+++ lc/344.cc
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string reverseString(string s) {
+        std::string result = std::string();
+        for (int i = s.length() - 1; i >= 0; --i) {
+            result.push_back(s[i]);
+        }
+        return result;
+    }
+};
