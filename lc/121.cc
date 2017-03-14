687e2c18f87ef31549d43f0c0bf6d99c5543f800
Sergei Radutnuy <sergeiradutnuy01@gmail.com>
2017-03-14 00:41:45 -0700

    121 6ms 45.03% c++

---
+++ lc/121.cc
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(const vector<int>& prices) {
+        int currentMin = INT_MAX;
+        int currentBest = 0;
+        for (const int& p : prices) {
+            if (p < currentMin) {
+                currentMin = p;
+            } else {
+                currentBest = max(p - currentMin, currentBest);
+            }
+        }
+        return currentBest;
+    }
+};
