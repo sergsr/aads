297aab79adf7d1305545d207d75ef3bcc7b3d004
Sergei Radutnuy <sergeiradutnuy01@gmail.com>
2017-03-14 00:38:20 -0700

    118 3ms 8.07% c++

---
+++ lc/118.cc
@@ -0,0 +1,24 @@
+class Solution {
+    vector<int> pascalRow(int n) {
+        vector<int> row;
+
+        int elem = 1;
+        for (int k = 0; k < n; ++k) {
+            row.push_back(elem);
+            elem *= (n - k - 1);
+            elem /= (k + 1);
+        }
+
+        return row;
+    }
+
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> rows;
+
+        for (int i = 1; i <= numRows; ++i) {
+            rows.push_back(pascalRow(i));
+        }
+        return rows;
+    }
+};
