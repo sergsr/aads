8adf58af7dd3b0fb3f8b67f55de33d33beb5be6d
Sergei Radutnuy <sergeiradutnuy01@gmail.com>
2017-03-14 00:40:23 -0700

    120 6ms 35.35% c++

---
+++ lc/120.cc
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        if (triangle.empty()) return 0;
+
+        vector<int> accum(triangle.back());
+        for (int i = triangle.size() - 2; i >= 0; --i) {
+            for (int j = 0; j < triangle[i].size(); ++j) {
+                accum[j] = triangle[i][j] + min(accum[j], accum[j+1]);
+            }
+        }
+        return accum[0];
+    }
+};
