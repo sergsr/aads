5c32756050eba2d837e184c8ba87d944c843b7aa
Sergei Radutnuy <sergeiradutnuy01@gmail.com>
2018-06-03 01:38:44 -0700

    fewer lines

--- rust/oiler/src/solutions.rs
+++ rust/oiler/src/solutions.rs
@@ -8,38 +8,33 @@ pub fn largest_product_in_a_series(x: usize, digits: &Vec<u64>) -> Option<u64> {
         .max()
 }
 
+fn line_product(old: Option<u32>, step: &Fn(usize) -> u32) -> Option<u32> {
+    old.max(Some((0..4).map(step).product()))
+}
+
 pub fn largest_product_in_a_grid(grid: &Vec<Vec<u32>>) -> Option<u32> {
     if grid.is_empty() {
         return None;
     }
-    let bottom = grid.len();
-    let right = grid[0].len();
 
-    (0..bottom)
+    (0..grid.len())
         .into_par_iter()
         .filter_map(|r| {
-            let go_down = r + 3 < bottom;
-            (0..right)
+            (0..grid[0].len())
                 .into_par_iter()
                 .filter_map(|c| {
-                    let go_right = c + 3 < right;
-                    let go_left = 2 < c;
                     let mut result: Option<u32> = None;
-                    if go_right {
-                        result = result.max(Some((0..4).map(|d| grid[r][c + d]).product()));
+                    if c + 3 < grid[0].len() {
+                        result = line_product(result, &|d| grid[r][c + d]);
                     }
-                    if go_down {
-                        result = result.max(Some((0..4).map(|d| grid[r + d][c]).product()));
+                    if r + 3 < grid.len() {
+                        result = line_product(result, &|d| grid[r + d][c]);
                     }
-                    if go_right && go_down {
-                        result = result.max(Some((0..4).map(|d| grid[r + d][c + d]).product()));
+                    if c + 3 < grid[0].len() && r + 3 < grid.len() {
+                        result = line_product(result, &|d| grid[r + d][c + d]);
                     }
-                    if go_left && go_down {
-                        result = result.max(Some(
-                            (0..4)
-                                .map(|d| grid[r + d][(c as isize - d as isize) as usize])
-                                .product(),
-                        ));
+                    if 2 < c && r + 3 < grid.len() {
+                        result = line_product(result, &|d| grid[r + d][c - d]);
                     }
                     return result;
                 })
