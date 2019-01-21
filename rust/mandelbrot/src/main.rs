af1be8e85592fe2b6f195b12abfd6ffaf138e792
Sergei Radutnuy <sergeiradutnuy01@gmail.com>
2019-01-21 14:44:39 -0800

    switch to floats and cargo fmt

--- rust/mandelbrot/src/main.rs
+++ rust/mandelbrot/src/main.rs
@@ -8,15 +8,13 @@ extern crate num_cpus;
 //extern crate serde_derive;
 
 use image::ColorType;
-use image::png::PNGEncoder;
 use num::Complex;
-use std::fs::File;
 use std::io::Write;
 use std::str::FromStr;
 
 /*
 const USAGE: &'static str = "\
-mandelbrot. 
+mandelbrot.
 
 Usage:
     mandelbrot <filename> <width> <height> <ulr> <uli> <lrr> <lri>
@@ -36,7 +34,7 @@ struct Args {
 */
 
 /// this is a documentation comment
-fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
+fn escape_time(c: Complex<f32>, limit: u32) -> Option<u32> {
     let mut z = Complex::new(0.0, 0.0);
     for i in 0..limit {
         z = z * z + c;
@@ -48,55 +46,54 @@ fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
 }
 
 fn pixel_to_point(
-    bounds: (usize, usize), 
-    pixel: (usize, usize), 
-    upper_left: Complex<f64>, 
-    lower_right: Complex<f64>
-) -> Complex<f64> 
-{
-    let (width, height) = (lower_right.re - upper_left.re, upper_left.im - lower_right.im);
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f32>,
+    lower_right: Complex<f32>,
+) -> Complex<f32> {
+    let (width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
     Complex::new(
-        upper_left.re + pixel.0 as f64 * width / bounds.0 as f64, 
-        upper_left.im - pixel.1 as f64 * height / bounds.1 as f64)
+        upper_left.re + pixel.0 as f32 * width / bounds.0 as f32,
+        upper_left.im - pixel.1 as f32 * height / bounds.1 as f32,
+    )
 }
 
 fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     match s.find(separator) {
         None => None,
-        Some(index) => {
-            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
-                (Ok(l), Ok(r)) => Some((l, r)),
-                _ => None,
-            }
-        }
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
     }
 }
 
-fn parse_complex(s: &str) -> Option<Complex<f64>> {
+fn parse_complex(s: &str) -> Option<Complex<f32>> {
     match parse_pair(s, ',') {
         Some((re, im)) => Some(Complex::new(re, im)),
         None => None,
     }
 }
 
-fn render(pixels: &mut [u8], bounds: (usize, usize), upper_left: Complex<f64>, lower_right: Complex<f64>) {
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f32>,
+    lower_right: Complex<f32>,
+) {
     assert!(pixels.len() == bounds.0 * bounds.1);
     for row in 0..bounds.1 {
         for column in 0..bounds.0 {
             let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
             pixels[row * bounds.0 + column] = match escape_time(point, 255) {
                 None => 0,
-                Some(count) => 255 - count as u8
+                Some(count) => 255 - count as u8,
             };
         }
     }
-}
-
-fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> std::io::Result<()> {
-    let output = File::create(filename)?;
-    let encoder = PNGEncoder::new(output);
-    encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Palette(8))?;
-    Ok(())
 }
 
 fn main() {
@@ -133,14 +130,23 @@ fn main() {
                 let end = band.len() / width;
                 let band_bounds = (width, end);
                 let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
-                let band_lower_right = pixel_to_point(bounds, (width, top + end), upper_left, lower_right);
+                let band_lower_right =
+                    pixel_to_point(bounds, (width, top + end), upper_left, lower_right);
                 // move makes closure takes ownership of all variables it uses
                 // only the closure may use mutable slice band
                 spawner.spawn(move |_| {
                     render(band, band_bounds, band_upper_left, band_lower_right);
                 });
             }
-        }).unwrap();
+        })
+        .unwrap();
     }
-    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
+    image::save_buffer(
+        &args[1],
+        &pixels,
+        width as u32,
+        height as u32,
+        ColorType::Gray(8),
+    )
+    .expect("saving buffer failed");
 }
